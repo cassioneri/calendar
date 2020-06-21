9b30c1239bc116a26ed4cb85a51244a64922bf2e
Cassio Neri <cassio.neri@gmail.com>
2020-06-21 16:28:20 +0100

    Replaced brute force coefficients search with Troesch's algorithm.

--- search.cpp
+++ search.cpp
@@ -17,145 +17,277 @@
  *
  **************************************************************************************************/
 
-#include <cstdint>
+#include <algorithm>
+#include <array>
 #include <iostream>
-#include <ratio>
-#include <utility>
-
-// Compile with: g++ -O3 -std=c++2a search.cpp -o search
-
-using integer_t = std::uint32_t;
 
 /**
- * Searcher of coefficients a, b and c such that f(n) = (a * n + b) / c gives results matching
- * expectations defined by a Tester class. For c, only powers of 2 are considered.
+ * @file Search.cpp
  *
- * For instance, for n in [0, 11] let g(n) = (153 * n + 2) / 5. This class searches for a, b and c
- * such c is a power of 2 and g(n) = f(n) for all n in [0, 11].
+ * Coefficients search algorithm by Troesch [1].
  *
- * @tparam Tester  Implements a static function test such that test(a, b, c)) == true if, and only
- *                 if, f mathes the expected results.
- * @tparam Hint    Lower bound hint for a / c. It must be an instantiation of std::ratio where
- *                 Hint::num == 1 or Hint::den == 1.
+ * Usage:
+ *
+ * search X1 X2 [Xn]...
+ *
+ * Tell if (X1, X2, ..., Xn) is the code of a line of not and, if so, then if also outputs the
+ * equation line. For instance, for the Gregorian months from March to February (regardless of leap
+ * year) we have
+ *
+ * $ search 31 30 31 30 31 31 30 31 30 31 31 30
+ * The line is y = (153 * x + 2) / 5.
+ *
+ * This means that (153 * x + 2) / 5 is the sum of all elememts of the vector {31, 30, 31, 30, 31,
+ * 31, 30, 31, 30, 31, 31, 30 } prior to index x.
+ *
+ * [1] Albert Troesch, Droites discrètes et calendriers, Mathématiques et sciences humaines, tome
+ *     141 (1998), p. 11-41.
+ *
+ * Compile with: g++ -O3 -std=c++2a search.cpp -o search
  */
-template <typename Tester, typename Hint>
-struct finder {
 
-  static_assert(Hint::num == 1 || Hint::den == 1);
+/**
+ * @brief Code container.
+ */
+using code_t = std::vector<int>;
 
-  integer_t static constexpr
-  calc(integer_t n, integer_t a, integer_t b, integer_t c) noexcept {
-    return (a * n + b) / c;
+/**
+ * @brief   Returns the difference between the maximum and the minimun of a code_t.
+ *
+ * @param   c         The given code.
+ *
+ * @pre               !c.empty();
+ */
+int amplitude(code_t const& c) {
+  auto const [iterator_min, iterator_max] = std::minmax_element(std::begin(c), std::end(c));
+  return *iterator_max - *iterator_min;
+}
+
+/**
+ * @brief   Returns true if code elements are in {x, x + 1} for some integer x.
+ *
+ * @param   c         The given code.
+ *
+ * @pre               !c.empty();
+ */
+int has_at_most_two_consecutives(code_t const& c) {
+  return amplitude(c) <= 1;
+}
+
+/**
+ * @brief   Returns true if all code elements are equal.
+ *
+ * @param   c         The given code.
+ *
+ * @pre               !c.empty();
+ */
+bool is_constant(code_t const& c) {
+  return amplitude(c) == 0;
+}
+
+/**
+ * @brief   Returns the code's minimum element.
+ *
+ * @param   c         The given code.
+ *
+ * @pre               !c.empty();
+ */
+int min_element(code_t const& c) {
+  return *min_element(std::cbegin(c), std::cend(c));
+}
+
+/**
+ * @brief   Subtracts a given number from all elements of a given code.
+ *
+ * @param   p         The given number.
+ * @param   c         The given code.
+ */
+void substract_element(int p, code_t& c) {
+  for (auto& y : c)
+    y -= p;
+}
+
+/**
+ * @brief   Returns true if the code does not contain two consecutive elements equal to 1.
+ *
+ * @param   c         The given code.
+ */
+bool is_1_isolated(code_t const& c) {
+  auto const size = std::size(c);
+  for (auto i = 1; i < size; ++i) {
+    if (c[i] == 1 && c[i - 1] == 1)
+      return false;
   }
+  return true;
+}
 
-  /**
-   * Brute force search for coefficients.
-   *
-   * It stops when coefficients are found or when combinations are exhausted. In the former case,
-   * it prints out the found coefficients.
-   */
-  void static constexpr
-  find() noexcept {
+/**
+ * @brief   Replace all 1 elements of a given code with 0 and vive versa.
+ *
+ * @param   c         The given code.
+ *
+ * @pre               c[i] == 1 || c[i] == 0 for all i in {0, ..., c.size() - 1}
+ */
+void swap_0_1(code_t& c) {
+  for (auto& y : c)
+    y = 1 - y;
+}
 
-    for (integer_t c = 1; ; ) {
+/**
+ * @brief   Replace code elements with plateau lengths.
+ *
+ * @param   c         The given code_t.
+ *
+ * @pre               c[i] == 1 || c[i] == 0 for all i in {0, ..., c.size() - 1}
+ */
+auto replace_with_lengths(code_t& c) {
 
-      auto const a_min = (c + Hint::den - 1) / Hint::den; // = ceil(c * num / den)
-      auto const a_max = Hint::den == 1 ? c * (Hint::num + 1) : c / (Hint::den - 1);
+  auto const size = std::size(c);
 
-      for (integer_t a = a_min; a < a_max; ++a) {
-        for (integer_t b = 0; b < a; ++b) {
-          if (Tester::test(a, b, c)) {
-            std::cout << "a = " << a << ", b = " << b << ", c = " << c << ".\n";
-            return;
-          }
-        }
+  int index_previous_1 = -1;
+  int first_length     = -1;
+  int n_plateaus       = 0;
+  int min_lenght       = size; // works as if +infinity
+
+  for (int i = 0; i < size; ++i) {
+    if (c[i] == 1) {
+      ++n_plateaus;
+      auto const length = i - index_previous_1;
+      index_previous_1 = i;
+      if (n_plateaus == 1)
+        first_length = length;
+      else if (length > min_lenght)
+          min_lenght = length;
+    }
+  }
+  auto const is_terminal = c.back() == 0;
+  n_plateaus += is_terminal;
+  int last_length = is_terminal ? size - index_previous_1 : 0;
+
+  code_t lengths;
+
+  if (n_plateaus > 2) {
+
+    auto const skip_first = first_length <= min_lenght;
+    if (!skip_first)
+      lengths.push_back(first_length);
+
+    index_previous_1 = first_length - 1;
+    for (int i = first_length; i < size; ++i) {
+      if (c[i] == 1) {
+        lengths.push_back(i - index_previous_1);
+        index_previous_1 = i;
       }
-
-      if (c == (~integer_t(0)) / 2 + 1)
-        return;
-
-      c <<= 1;
     }
+
+    if (last_length > min_lenght)
+      lengths.push_back(first_length);
+
+    c = std::move(lengths);
+    return skip_first ? first_length : 0;
   }
+
+  if (n_plateaus == 1 || first_length >= last_length) {
+    c.resize(1);
+    c[0] = first_length;
+    return 0;
+  }
+
+  c.resize(1);
+  c[0] = last_length;
+  return first_length;
+}
+
+/**
+ * Return the remainder of Euclidean division of n by d.
+ *
+ * @param   n         The dividend.
+ * @param   d         The divisor.
+ *
+ * @pre               d != 0.
+ */
+auto mod(int n, int d) {
+  auto r = n % d;
+  if (r < 0)
+    r += d > 0 ? d: - d;
+  return r;
+}
+
+/**
+ * @brief Result of Troesch's algorithm.
+ */
+struct result_t {
+  bool is_line;
+  int  a;
+  int  b;
+  int  r;
 };
 
 /**
- * For n = [0, 11], let m = n > 9 ? n - 9 : n + 3. Let d denote the first day of month m and e
- * denote the 1st of March preceding or on d. The mounth count, f(n) is defined by the number of
- * dates in [e, d[. It is known that g(n) = (153 * n + 2) / 5. This class is to be used as a Tester
- * class of finder that tests for month count.
+ * @brief Runs Troesch's algorithm on a given a code.
  */
-struct month_count {
+result_t troesch(code_t& c) {
 
-  using finder = ::finder<month_count, std::ratio<30, 1>>;
+  std::vector<char> e; // avoiding vector<bool>;
+  code_t p, g;
+  int n;
 
-  integer_t static constexpr
-  calc(integer_t n) noexcept {
-    return (153 * n + 2) / 5;
-  }
+  auto increment_n = [&]() {
+    ++n;
+    p.resize(n + 1);
+    e.resize(n + 1);
+    g.resize(n + 1);
+  };
 
-  bool static constexpr
-  test(integer_t a, integer_t b, integer_t c) noexcept {
-    for (integer_t n = 0; n < 12; ++n)
-      if (finder::calc(n, a, b, c) != calc(n))
-        return false;
-    return true;
-  }
-};
-
-/**
- * Search coefficients for function that returns the month for a given day of year.
- */
-struct month {
-
-  using finder = ::finder<month, std::ratio<1, 31>>;
-
-  bool static constexpr
-  test(integer_t a, integer_t b, integer_t c) noexcept {
-    for (integer_t n = 0; n < 12; ++n) {
-      // The month is m = n > 9 ? n - 9 : n + 3
-      if (finder::calc(month_count::calc(n), a, b, c) != n ||
-        finder::calc(month_count::calc(n + 1) - 1, a, b, c) != n)
-        return false;
+  bool is_line = has_at_most_two_consecutives(c);
+  n = 0;
+  while (is_line && !is_constant(c)) {
+    increment_n();
+    p[n] = min_element(c);
+    substract_element(p[n], c);
+    e[n] = !is_1_isolated(c);
+    if (e[n]) {
+      increment_n();
+      swap_0_1(c);
     }
-    return true;
-  }
-};
-
-/**
- * Search coefficients for function that returns the year for a given day of century.
- */
-struct year_of_century {
-
-  using finder = ::finder<year_of_century, std::ratio<1, 366>>;
-
-  integer_t static constexpr
-  year_count(integer_t y) noexcept {
-    return 1461 * y / 4;
+    increment_n();
+    g[n] = replace_with_lengths(c);
+    is_line = has_at_most_two_consecutives(c);
   }
 
-  bool static constexpr
-  test(integer_t a, integer_t b, integer_t c) noexcept {
-    for (integer_t y = 0; y < 100; ++y)
-      if (finder::calc(year_count(y), a, b, c) != y ||
-        finder::calc(year_count(y + 1) - 1, a, b, c) != y)
-        return false;
-    return true;
+  if (!is_line)
+    return { false, 0, 0, 0 };
+
+  auto a = c[0];
+  auto b = 1;
+  auto r = 0;
+  while (n > 0) { // Error in Troesch's article: while (n >= 0)
+    --n;
+    std::swap(a, b);
+    r = a - 1 - r;
+    r = mod(r - g[n + 1] * a, b);
+    if (e[n - 1]) { // Error in Troesch's article: if (e[n])
+      --n;
+      a = b - a;
+      r = b - 1 - r;
+    }
+    --n;
+    a = a + p[n + 1] * b;
   }
-};
+  return { true, a, b, r };
+}
 
-int main() {
+int main(int argc, char* argv[]) {
 
-  std::cout << "Coefficients for month_count: ";
-  // Result: a = 979, b =  15, c = 32.
-  month_count::finder::find();
+  code_t c;
 
-  std::cout << "Coefficients for month from day of year: ";
-  // Result: a = 535, b = 331, c = 16384.
-  month::finder::find();
+  for (int i = 1; i < argc; ++i)
+    c.push_back(std::atoi(argv[i]));
 
-  std::cout << "Coefficients for year of century: ";
-  // For integer_t = std::uint32_t finishes after around 15min and no coefficients are found.
-  // For integer_t = std::uint64_t cannot find coefficients in a reasonable amount of time.
-  year_of_century::finder::find();
+  auto results = troesch(c);
+  if (results.is_line)
+    std::cout << "The line is y = (" << results.a << " * x + " << results.r << ") / " <<
+      results.b << ".\n";
+  else
+    std::cout << "This is not the code of a line.\n";
 }
