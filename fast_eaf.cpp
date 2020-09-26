9a8ef18d8c592dfb16233dcb8555707c7124ec26
Cassio Neri <cassio.neri@gmail.com>
2020-09-26 01:42:46 +0100

    Unique fast_eaf implementation.

--- fast_eaf.cpp
+++ fast_eaf.cpp
@@ -25,11 +25,37 @@
  * Compile with: g++ -O3 -std=c++2a fast_eaf.cpp -o fast_eaf
  */
 
-#include "fast_eaf.hpp"
-
 #include <algorithm>
+#include <cstring>
 #include <iostream>
-#include <cstdint>
+
+/**
+ * @brief Coefficients of EAF.
+ */
+struct eaf_t {
+  std::int64_t alpha;
+  std::int64_t beta;
+  std::int64_t delta;
+};
+
+/**
+ * @brief Coefficients and upper bound of fast EAFs.
+ */
+struct fast_eaf_t {
+  eaf_t        fast;
+  std::int32_t k;
+  std::int64_t upper_bound;
+};
+
+std::ostream& operator <<(std::ostream& os, fast_eaf_t const& eaf) {
+  return os <<
+    "alpha'      = " << eaf.fast.alpha  << "\n"
+    "beta'       = " << eaf.fast.beta   << "\n"
+    "delta'      = " << eaf.fast.delta  << "\n"
+    "k           = " << eaf.k           << "\n"
+    "upper bound = " << eaf.upper_bound << "\n"
+    ;
+}
 
 /**
  * @brief Finds coefficients and upper bound of fast EAF.
@@ -38,13 +64,12 @@
  * @param   eaf       Original EAF.
  */
 fast_eaf_t constexpr
-get_fast_eaf(std::int32_t k, eaf_t const& eaf) noexcept {
+get_fast_eaf(bool round_up, std::int32_t k, eaf_t const& eaf) noexcept {
 
   auto const two_k       = std::int64_t(1) << k;
   auto const two_k_alpha = two_k * eaf.alpha;
   auto const div         = two_k_alpha / eaf.delta;
   auto const mod         = two_k_alpha % eaf.delta;
-  auto const round_up    = mod > eaf.delta - mod;
   auto const alpha_prime = round_up ? div + 1 : div;
   auto const nu          = round_up ? eaf.delta - mod : mod;
   
@@ -96,12 +121,63 @@ get_fast_eaf(std::int32_t k, eaf_t const& eaf) noexcept {
     return { { alpha_prime, beta_prime, two_k }, k, N };
 }
 
+/**
+ * @brief Finds coefficients and upper bound of simple fast EAF.
+ * 
+ * @param   k         Exponent of the divisor.
+ * @param   eaf       Original EAF.
+ */
+fast_eaf_t constexpr
+get_simple_fast_eaf(std::int32_t k, eaf_t const& eaf) noexcept {
+  auto const two_k = std::int64_t(1) << k;
+  auto const mu    = two_k / eaf.delta + 1;
+  auto const nu    = eaf.delta - two_k % eaf.delta;
+  auto const N     = (mu / nu + (mu % nu != 0)) * eaf.delta - 1;
+  return { { mu * eaf.alpha, mu * eaf.beta, two_k }, k, (nu <= mu ? N : 0) };
+}
+
 int main(int argc, char* argv[]) {
 
-  auto eaf = parse_cmd_line_and_exit_if_invalid(argc, argv);
+  if (argc < 5) {
+    std::cerr << argv[0] << ": requires at least 5 arguments: method, alpha, beta, delta and k\n"; 
+    exit (1);
+  }
+
+  enum class method_t { up, down, simple };
+  method_t method;
+  if (std::strncmp(argv[1], "up", 3) == 0)
+    method = method_t::up;
+  else if (std::strncmp(argv[1], "down", 5) == 0)
+    method = method_t::down;
+  else if (std::strncmp(argv[1], "simple", 7) == 0)
+    method = method_t::simple;
+  else {
+    std::cerr << argv[0] << ": unknown method '" << argv[1] << "'\n"; 
+    exit (1);
+  }
+
+  auto alpha = std::atoi(argv[2]);
+  auto beta  = std::atoi(argv[3]);
+  auto delta = std::atoi(argv[4]);
+
+  if (alpha <= 0 || delta <= 0) {
+    std::cerr << argv[0] << ": alpha and delta must be strictly positive.\n";
+    std::exit(1);
+  }
   
-  for (std::int32_t k = 1; k <= 32; ++k) {
-    auto const fast_eaf = get_fast_eaf(k, eaf);
+  auto const eaf = eaf_t{ alpha, beta, delta };
+  
+  for (std::int32_t i = 5; i < argc; ++i) {
+    
+    auto k = std::atoi(argv[i]);
+    if (k < 1 || k > 50) {
+      std::cerr << argv[0] << ": k must be in [1, 50] (skipping k = " << k << ")\n\n";
+      continue;
+    }
+    
+    auto const fast_eaf = (method == method_t::simple) ? get_simple_fast_eaf(k, eaf) :
+      get_fast_eaf(method == method_t::up, k, eaf);
+
     std::cout << fast_eaf << '\n';
   }
 }
